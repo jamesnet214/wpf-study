3bb4618e02d662b0bcb0bc9462b39d38e568f9e2
JAEWUNG LEE <james@jamesnet.dev>
2023-05-06 15:01:20 +0900

    Updates

---
+++ src/WpfPracticeApp/WpfPracticeApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPracticeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lbx.ItemsSource = GetSamples();
+        }
+
+        private IEnumerable GetSamples()
+        {
+            List<User> users = new();
+            users.Add(new User().GenData("James", "Seoul"));
+            users.Add(new User().GenData("Vicky", "Napoli"));
+            users.Add(new User().GenData("Harry", "Paris"));
+            users.Add(new User().GenData("Luke", "London"));
+
+            return users;
+        }
+    }
+}
---
+++ src/WpfPracticeApp/WpfPracticeApp/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpfPracticeApp
+{
+    internal class User
+    {
+        public string Name { get; private set; }
+        public string Location { get; private set; }
+
+        internal User GenData(string name, string location)
+        {
+            var user = new User();
+            user.Name = name;
+            user.Location = location;
+            return user;
+        }
+    }
+}
