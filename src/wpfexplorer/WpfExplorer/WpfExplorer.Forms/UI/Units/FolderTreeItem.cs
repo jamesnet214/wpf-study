d0bdd6649ecfaedea48d9ec39ff70f6c3d70b507
JAEWUNG LEE <james@jamesnet.dev>
2023-09-02 14:14:17 +0900

    Updates

---
+++ src/wpfexplorer/WpfExplorer/WpfExplorer.Forms/UI/Units/FolderTreeItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Forms.UI.Units
+{
+    public class FolderTreeItem : TreeViewItem
+    {
+        static FolderTreeItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FolderTreeItem), new FrameworkPropertyMetadata(typeof(FolderTreeItem)));
+        }
+    }
+}
---
+++ src/wpfexplorer/WpfExplorer/WpfExplorer.Forms/UI/Units/FolderTreeView.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Forms.UI.Units
+{
+    public class FolderTreeView : TreeView
+    {
+        static FolderTreeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FolderTreeView), 
+                new FrameworkPropertyMetadata(typeof(FolderTreeView)));
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new FolderTreeItem();
+        }
+    }
+}
